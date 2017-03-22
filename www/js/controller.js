f6fca058161dc29173c65c7bf9ebaead98b7d3fd
codev <codev@codevs-iMac.local>
2017-03-21 18:56:29 -0600

    login page design

---
+++ www/js/controller.js
@@ -0,0 +1,85 @@
+/**
+ * Created by codev on 3/21/17.
+ */
+angular.module('starter.controllers', ['ionic.cloud'])
+
+  .controller('loginCtrl', function($scope, $ionicAuth, $ionicUser, $state) {
+
+    $scope.user = {
+      firstName: null,
+      lastName: null,
+      email: null,
+      password: null
+    };
+
+    var details = {};
+
+
+    $scope.signupUser = function(firstName, lastName, email, password) {
+
+
+
+      details.name = firstName + " " + lastName
+      details.email = email
+      details.password = password
+
+      console.log('log out details')
+      console.log(details)
+
+      // check variables and sign up user
+      if (details.name && details.email && details.password) {
+        $ionicAuth.login('basic', details).then(function() {
+          // `$ionicUser` is now registered
+          console.log('i am signed in')
+          if ($ionicAuth.isAuthenticated()) {
+            // $ionicUser is authenticated!
+            console.log('i am passed')
+            $state.go('home');
+
+          }
+        });
+
+
+      }
+
+    }
+
+
+
+
+  })
+
+
+//   .controller('ChatDetailCtrl', function($scope, $stateParams, Chats, $ionicDB) {
+//     $scope.chat = Chats.get($stateParams.chatId);
+//
+//     var messages = $ionicDB.collection('messages')
+//
+// // Establish the db connection.
+//     $ionicDB.connect();
+//
+//     $ionicDB.status().subscribe(function(status) {
+//       if (status.type === 'disconnected') {
+//         // Display connecting spinner
+//         console.log(status.type); // reconnecting
+//       }
+//     });
+//
+// // Equivalent to
+//     $ionicDB.onReconnecting().subscribe(function(status) {
+//       // Display connecting spinner
+//       console.log(status.type); // reconnecting
+//     });
+//
+// // Start using the collection
+//     messages.store({ msg: 'Hello world!' });
+//
+//
+//   })
+
+  .controller('homeCtrl', function($scope) {
+
+
+
+
+  });
